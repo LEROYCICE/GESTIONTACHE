b05afa1280ad2fe92c228016aaaf45807b288b40
LEROY <kokoukpomblawou@gmail.com>
2022-10-27 01:06:59 +0200

    "Le CRUD des taches"

---
+++ resources/views/affichage_tache.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+
+@section('content')
+
+<div class="card">
+    @foreach ($taches as $tache)
+        
+    @endforeach
+    <header class="card-header">
+      <p class="card-header-title">
+        {{$tache->titer}}
+      </p>
+      <button class="card-header-icon" aria-label="more options">
+      </button>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        {{$tache->contenu}}
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="{{route('show')}}" class="card-footer-item"> Voir</a>
+      <a href="{{route('edit')}}" class="card-footer-item">Modifier</a>
+      <a href="{{route('delete')}}" class="card-footer-item">Supprimer</a>
+    </footer>
+  </div>
+    
+@endsection
